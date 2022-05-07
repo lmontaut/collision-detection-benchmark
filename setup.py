670f187fd3dc2fb1604c9c1e9791f7db6ad7def6
lmontaut <louismontaut@gmail.com>
2022-05-07 15:17:28 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name="pycolbench",
+    version='1.0',
+    description="Collision detection benchmarks for Nesterov-accelerated GJK.",
+    packages=['pycolbench'],
+    author='Louis Montaut',
+    author_email='louismontaut@gmail.com',
+)
